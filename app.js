7bfc419e575a69cf41f912a9395fe4d20f043ce2
AyrDS <ayrtonds97@gmail.com>
2021-08-09 23:17:44 -0300

    Proyecto finalizado

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const formulario = document.querySelector("#nuevo-turno");
 /* ------------------ */
 
 let editando;
+let DB;
 
 /* CLASES */
 class Turnos {
@@ -21,10 +22,6 @@ class Turnos {
 
     agregarTurno(turno) {
         this.turnos = [...this.turnos, turno];
-    }
-
-    eliminarTurno(id) {
-        this.turnos = this.turnos.filter(turno => turno.id !== id);
     }
 
     editarTurno(turnoActualizado) {
@@ -52,71 +49,81 @@ class UI {
         }, 3000);
     };
 
-    mostrarTurnos({ turnos }) {
+    mostrarTurnos() {
         this.limpiarHTML();
-        document.querySelector(".tituloTurnos").style.display = "none";
         console.log(turnos);
 
-        turnos.forEach(turno => {
-            const { nombre, telefono, fecha, hora, sintomas, id } = turno;
+        //Leer contenido de la base de datos
+        const objectStore = DB.transaction("turnos").objectStore("turnos");
 
-            const divTurno = document.createElement("div");
-            divTurno.classList.add("turno", "card", "p-3", "mb-3");
-            divTurno.dataset.id = id;
+        const fnTextoTitulo = this.textoTitulo;
+        const total = objectStore.count();
+        total.onsuccess = function () {
+            fnTextoTitulo(total.result);
+        }
+        objectStore.openCursor().onsuccess = function (e) {
+            const cursor = e.target.result;
 
-            const nombreParrafo = document.createElement("h3");
-            nombreParrafo.classList.add("card-title", "fw-bolder");
-            nombreParrafo.textContent = nombre;
+            if (cursor) {
+                const { nombre, telefono, fecha, hora, sintomas, id } = cursor.value;
 
-            const telefonoParrafo = document.createElement("p");
-            telefonoParrafo.innerHTML = `
-                <span class="fw-bolder">Teléfono: </span>${telefono}
-            `;
+                const divTurno = document.createElement("div");
+                divTurno.classList.add("turno", "card", "p-3", "mb-3");
+                divTurno.dataset.id = id;
 
-            const fechaParrafo = document.createElement("p");
-            fechaParrafo.innerHTML = `
-                <span class="fw-bolder">Fecha: </span>${fecha}
-            `;
+                const nombreParrafo = document.createElement("h3");
+                nombreParrafo.classList.add("card-title", "fw-bolder");
+                nombreParrafo.textContent = nombre;
 
-            const horaParrafo = document.createElement("p");
-            horaParrafo.innerHTML = `
-                <span class="fw-bolder">Hora: </span>${hora}
-            `;
+                const telefonoParrafo = document.createElement("p");
+                telefonoParrafo.innerHTML = `
+                <span class="fw-bolder">Teléfono: </span>${telefono}`;
 
-            const sintomasParrafo = document.createElement("p");
-            sintomasParrafo.innerHTML = `
-                <span class="fw-bolder">Sintomas: </span>${sintomas}
-            `;
+                const fechaParrafo = document.createElement("p");
+                fechaParrafo.innerHTML = `
+                <span class="fw-bolder">Fecha: </span>${fecha}`;
 
-            //Botones
-            const divBotones = document.createElement("div");
-            divBotones.classList.add("d-grid", "gap-2", "d-md-block");
+                const horaParrafo = document.createElement("p");
+                horaParrafo.innerHTML = `
+                <span class="fw-bolder">Hora: </span>${hora}`;
 
-            //Boton para eliminar
-            const btnEliminar = document.createElement("button");
-            btnEliminar.classList.add("btn", "btn-danger", "mt-2");
-            btnEliminar.innerHTML = "Eliminar <i class='fas fa-trash-alt'></i>";
-            btnEliminar.onclick = () => eliminarTurno(id);
-            divBotones.appendChild(btnEliminar);
+                const sintomasParrafo = document.createElement("p");
+                sintomasParrafo.innerHTML = `
+                <span class="fw-bolder">Sintomas: </span>${sintomas}`;
 
-            //Boton para editar
-            const btnEditar = document.createElement("button");
-            btnEditar.classList.add("btn", "btn-info", "mt-2", "ms-2");
-            btnEditar.innerHTML = "Editar <i class='fas fa-pen'></i>";
-            btnEditar.onclick = () => cargarEdicion(turno);
-            divBotones.appendChild(btnEditar);
+                //Botones
+                const divBotones = document.createElement("div");
+                divBotones.classList.add("d-grid", "gap-2", "d-md-block");
+
+                //Boton para eliminar
+                const btnEliminar = document.createElement("button");
+                btnEliminar.classList.add("btn", "btn-danger", "mt-2");
+                btnEliminar.innerHTML = "Eliminar <i class='fas fa-trash-alt'></i>";
+                btnEliminar.onclick = () => eliminarTurno(id);
+                divBotones.appendChild(btnEliminar);
+
+                //Boton para editar
+                const btnEditar = document.createElement("button");
+                btnEditar.classList.add("btn", "btn-info", "mt-2", "ms-2");
+                btnEditar.innerHTML = "Editar <i class='fas fa-pen'></i>";
+                const turno = cursor.value;
+                btnEditar.onclick = () => cargarEdicion(turno);
+                divBotones.appendChild(btnEditar);
 
 
-            divTurno.appendChild(nombreParrafo);
-            divTurno.appendChild(telefonoParrafo);
-            divTurno.appendChild(fechaParrafo);
-            divTurno.appendChild(horaParrafo);
-            divTurno.appendChild(sintomasParrafo);
-            divTurno.appendChild(divBotones);
+                divTurno.appendChild(nombreParrafo);
+                divTurno.appendChild(telefonoParrafo);
+                divTurno.appendChild(fechaParrafo);
+                divTurno.appendChild(horaParrafo);
+                divTurno.appendChild(sintomasParrafo);
+                divTurno.appendChild(divBotones);
 
-            contenedorTurnos.appendChild(divTurno);
+                contenedorTurnos.appendChild(divTurno);
 
-        });
+                //Siguiente elemento de la db
+                cursor.continue();
+            }
+        }
     };
 
     limpiarHTML() {
@@ -124,6 +131,15 @@ class UI {
             contenedorTurnos.removeChild(contenedorTurnos.firstChild);
         }
     };
+
+    textoTitulo(resultado) {
+        const titulo = document.querySelector(".tituloTurnos")
+        if (resultado > 0) {
+            titulo.textContent = "Administra tus pacientes"
+        } else {
+            titulo.textContent = "No hay turnos reservados"
+        }
+    }
 }
 
 //Inicializamos las clases
@@ -140,6 +156,8 @@ document.addEventListener("DOMContentLoaded", () => {
     sintomasInput.addEventListener("input", datosTurno);
 
     formulario.addEventListener("submit", nuevoTurno);
+
+    crearDB();
 });
 
 const turnoObj = {
@@ -176,10 +194,24 @@ function nuevoTurno(e) {
         //Pasar el objeto actualizado
         administrarTurnos.editarTurno({ ...turnoObj });
 
-        ui.mostrarAlerta("Editado correctamente");
+        //Edita en IndexedDB
+        const transaction = DB.transaction(["turnos"], "readwrite");
+        const objectStore = transaction.objectStore("turnos");
 
-        formulario.querySelector("button[type='submit']").textContent = "Crear turno";
-        editando = false;
+        objectStore.put(turnoObj);
+
+        transaction.oncomplete = () => {
+
+            ui.mostrarAlerta("Editado correctamente");
+
+            formulario.querySelector("button[type='submit']").textContent = "Crear turno";
+            editando = false;
+        }
+
+        transaction.onerror = () => {
+            console.log("Hubo un error");
+        }
+
 
     } else {
 
@@ -187,10 +219,24 @@ function nuevoTurno(e) {
         turnoObj.id = Date.now();
 
         //Agregamos el turno al array de Turnos
-        administrarTurnos.agregarTurno({ ...turnoObj });        
+        administrarTurnos.agregarTurno({ ...turnoObj });
 
-        //Mensaje de agregado correctamente
-        ui.mostrarAlerta("Turno agregado correctamente")
+        //Insertar registro en indexedDB
+        const transaction = DB.transaction(["turnos"], "readwrite");
+
+        const objectStore = transaction.objectStore("turnos");
+
+        objectStore.add(turnoObj);
+
+        transaction.oncomplete = () => {
+            //Mensaje de agregado correctamente
+            ui.mostrarAlerta("Turno agregado correctamente")
+        }
+
+        transaction.onerror = () => {
+            console.log("Hubo un error");
+        }
+
     }
 
     //Reinicio del obj para próximas validaciones
@@ -199,7 +245,7 @@ function nuevoTurno(e) {
     formulario.reset();
 
     //Mostrar HTML
-    ui.mostrarTurnos(administrarTurnos);
+    ui.mostrarTurnos();
 }
 
 function reinicarObjeto() {
@@ -212,17 +258,20 @@ function reinicarObjeto() {
 
 function eliminarTurno(id) {
     //Eliminar turno
-    administrarTurnos.eliminarTurno(id);
+    const transaction = DB.transaction(["turnos"], "readwrite");
+    const objectStore = transaction.objectStore("turnos");
 
-    //Mostrar mensaje
-    ui.mostrarAlerta("El turno se eliminó correctamente");
+    objectStore.delete(id);
 
-    //Actualizar turnos
-    ui.mostrarTurnos(administrarTurnos);
-    const cards = document.querySelectorAll(".card");
-    if (cards.length <= 0) {
-        document.querySelector(".tituloTurnos").style.display = "block";
-    };
+    transaction.oncomplete = () => {
+
+        //Mostrar mensaje
+        ui.mostrarAlerta("El turno se eliminó correctamente");
+
+        //Actualizar turnos
+        ui.mostrarTurnos();
+    }
+
 }
 
 function cargarEdicion(turno) {
@@ -247,4 +296,39 @@ function cargarEdicion(turno) {
     formulario.querySelector("button[type='submit']").textContent = "Guardar cambios";
 
     editando = true;
+}
+
+function crearDB() {
+    const crearDB = window.indexedDB.open("turnos", 1);
+
+    crearDB.onerror = function () {
+        console.log("Error al crear base de datos");
+    };
+
+    crearDB.onsuccess = function () {
+        DB = crearDB.result;
+
+        //Mostrar lo que este guardado en la db una vez que este lista
+        ui.mostrarTurnos();
+    }
+
+    //Definir el schema
+    crearDB.onupgradeneeded = function (e) {
+        const db = e.target.result;
+
+        const objectStore = db.createObjectStore("turnos", {
+            keyPath: "id",
+            autoIncrement: true
+        });
+
+        //Definir columnas de la db
+        objectStore.createIndex("nombre", "nombre", { unique: false });
+        objectStore.createIndex("telefono", "telefono", { unique: false });
+        objectStore.createIndex("fecha", "fecha", { unique: false });
+        objectStore.createIndex("hora", "hora", { unique: false });
+        objectStore.createIndex("sintomas", "sintomas", { unique: false });
+        objectStore.createIndex("id", "id", { unique: true });
+
+        console.log("db creada");
+    }
 }
